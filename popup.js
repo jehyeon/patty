c1085fd53514b20284bc20192a40bacecb3249d3
hyeon62 <hyeon.rot@gmail.com>
2019-05-13 02:08:27 +0900

    Set google translate api but not working because CORS

--- popup.js
+++ popup.js
@@ -1,6 +1,30 @@
+// 항상 동작
+
 // 일단 'h1'에서만 정보 가져오기
 $('h1').mouseup(function() {
   if (window.getSelection) {
-    alert(window.getSelection());
+    // alert(window.getSelection());
+    // for test ->
+    translateThis(window.getSelection());
+    // 드래그 시 해당 pos 상단에 option 창 띄우기 (translate, save)
   }
 });
+
+function translateThis(wantToTranslate) {
+  // 언어 설정은 추후 update
+  const source = 'en';
+  const target = 'ko';
+  const format = 'html';
+  const apiKey = '********';
+
+  $.ajax({
+    type: 'POST',
+    data: '&source=' + source + '&target=' + target + '&format=' + format + '&q=' + wantToTranslate,
+    url: 'https://www.googleapis.com/language/translate/v2?key=' + apiKey,
+    success:function(response) {
+      alert(response);
+      // DOM 조작
+    }
+  });
+}
+
