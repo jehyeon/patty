d6fce1e7b464d34caf8d1652f92d668dec9cdf4a
jehyeon <hyeon.rot@gmail.com>
2019-05-16 18:26:50 +0900

    Update temporary popup view

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function (data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function (data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
 
 // changeColor.onClick = function (element) {
 //   let color = element.target.value;
