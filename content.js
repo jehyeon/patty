60ad0768b194886a859a32c7e678d33f94dab49d
jehyeon <hyeon.rot@gmail.com>
2019-05-16 17:28:36 +0900

    Implement temporary function

--- content.js
+++ content.js
@@ -1,45 +1,64 @@
 // for test
-const bubble = $('<span />');
+const bubble = $('<span/>');
 const bubble_css = {
   'position': 'absolute',
-  'margin-top': '-1.5em',
-  'background': 'red',
-  'border': '1px solid red',
-  'border-radius': '5px',
+  'margin-top': '-2em',
+  'background': 'black',
+  'color': 'white',
+  'font-size': '12pt',
+  'height': '1em',
+  'line-height': '1em',
+  'padding': '10px',
+  'border-radius': '20px',
   'z-index': 9999
 };
-
+var target;
+var originText = target;
+var dragging = false;
 // 항상 동작
 // 번역 모드 on 인 경우 
 $('*').mouseup(function(e) {
   e.stopPropagation();      // 여러 번 호출되는 것을 prevent (* selctor is one or more elements)
 
-  if (window.getSelection) {
+  console.log(window.getSelection().toString()); 
+  if (window.getSelection && window.getSelection().toString().length > 1) {
     // window.getSelection().toString().length < 500 인 경우
     // console.log('raw');
     // console.log(window.getSelection());
-    const draged = window.getSelection().toString();
+    if (dragging == true) {
+      // target.html(originText);
+      $('span.dragged').remove();
+      dragging = false;
+    }
+    dragging = true;
+    const dragged = window.getSelection().toString();
     
-    const target = $(this);
-    const text = target.html();
+    target = $(this);
+    originText = target.html();
+
+    console.log(dragged);
+    // drag한 text에 class 씌우기
+    
     // + 여러 영역 드래그 시 처음 문단만 쪼개는 기능 
     // + 온전하지 못한 문장 시 삭제하기 
     // ex. 'Simple one-time requests' -> 'ple one-time reque' -> 'one-time'
 
     // Text 터치 시 POST 하지 않도록 temp condtion
     // window.getSelection이 아닌 정확히 드래그 할 때만 동작하도록 수정 필요
-    if (window.getSelection().toString().length > 2) {
-      chrome.runtime.sendMessage({before: draged}, function(response) {
+    // if (window.getSelection().toString().length > 2) {
+      chrome.runtime.sendMessage({before: dragged}, function(response) {
         if (response) {
           console.log('response is ' + response.response);
-          bubble.text(response.response).css(bubble_css);
-          target.prepend(bubble);
+          // bubble.text(response.response).css(bubble_css);
+          
+          target.html(originText.replace(dragged, "<span class='dragged'>" + response.response + "</span>"  + dragged));
+          $('span.dragged').css(bubble_css);
+          // bubble.insertBefore($('span.dragged'));
           // target.html(text.replace(draged, response.response));
         } else {
           console.log('No reponse')
         }
       });
-    }
     // translateThis(window.getSelection());
     // 드래그 시 해당 pos 상단에 option 창 띄우기 (translate, save)
   }
